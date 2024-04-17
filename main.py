6c19cd2e8cc090b218aad37c9a9ee3f043fdfe90
Devansh Khare <devanshkhare0@gmail.com>
2024-04-17 22:10:12 +0530

    basic chain created

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import openai
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv()
 
 llm = ChatOpenAI()
 
-response = llm.invoke("What is the capital of india?")
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "Tell me the currency of countries that I will ask"),
+    ("user", "{input}")
+])
+
+chain = prompt | llm  
+
+response = chain.invoke({"input": "india"})
+
+
 print(response.content)
+
